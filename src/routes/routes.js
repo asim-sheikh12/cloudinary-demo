b5db82715ed75aa128f8b8c208cfa4d7958af804
asim-thoughtwin <asim.thoughtwin@gmail.com>
2022-05-09 15:44:32 +0530

    "Second commit"

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cloudUpload = require("../controller/controller");
-const Upload = require("../models/uploadModel");
 const router = express.Router();
 const upload = require("../middleware/multer");
-const saveURL = require("../middleware/saveURL");
 router.post("/upload-images", upload.array("image"), cloudUpload);
 module.exports = router;
